e880b034cd8ac76c821a710b89e9766e1f4a801a
paulq1984 <paulquinnell@gmail.com>
2024-11-27 14:40:21 -0500

    Dayview ideas

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
         <MapView/>
+        <DayView/>
     </div>
     
   )
--- src/components/DayView.jsx
+++ src/components/DayView.jsx
@@ -2,24 +2,191 @@ import React from 'react'
 
 const DayView = () => {
   return (
-    <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
-        <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
-            <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
-                <div className="bg-blue-500 text-white py-1">
-                    March
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
                 </div>
-                <div className="pt-1 border-l border-r border-white bg-white">
-                    <span className="text-5xl font-bold leading-tight">
-                        17
-                    </span>
-                </div>
-                <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white pt-2 mb-1">
-                    <span className="text-sm">
-                        Sunday
-                    </span>
-                </div>
+               </div>
             </div>
-        </div>  
+        </div>
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+        <div className="group cursor-pointer relative">
+            <div className="min-w-32 bg-white min-h-48 p-3 mb-4 font-medium">
+               <div className="w-32 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center shadow-lg">
+                <div className="block rounded-t overflow-hidden text-center">
+                    <div className="bg-blue-500 text-white py-1">
+                        March
+                    </div>
+                    <div className="pt-1 border-l border-r border-white bg-white">
+                        <span className="text-5xl font-bold leading-tight">
+                            28
+                        </span>
+                    </div>
+                    <div className="border-l border-r border-b rounded-b-lg text-center border-white bg-white -pt-2 -mb-1">
+                        <span className="text-sm">
+                            Sunday
+                        </span>
+                    </div>
+                    <div className="pb-2 border-l border-r border-b rounded-b-lg text-center border-white bg-white">
+                        <span className="text-xs leading-normal">
+                            6:00pm
+                        </span>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+
     </div>
   )
 }
